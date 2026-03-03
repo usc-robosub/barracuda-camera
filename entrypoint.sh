2a897e1b4d9a18dde5cf1fcdfb8449c71923d41b
uscauv26 <uscauv@gmail.com>
2026-03-03 15:46:59 -0800

    removed old docker files

--- entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-source /opt/ros/humble/setup.bash
-
-# Source ros2_ws
-source /opt/barracuda-camera/ros2_ws/install/local_setup.bash
-
-# Start camera node
-ros2 launch zed_wrapper zed_camera.launch.py camera_model:=zedm
